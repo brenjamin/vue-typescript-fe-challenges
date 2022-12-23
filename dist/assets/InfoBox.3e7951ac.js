2d66389e35fbf7f0260c2d352eaac3fc9503cfba
Ben <Ben@Bens-MacBook-Pro.local>
2022-12-22 19:36:19 -0500

    Updates for Netlify

--- dist/assets/InfoBox.3e7951ac.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as _,c as n,r}from"./index.32397322.js";const s={},t={class:"info-box"};function a(e,c){return _(),n("div",t,[r(e.$slots,"default")])}var l=o(s,[["render",a]]);export{l as I};
